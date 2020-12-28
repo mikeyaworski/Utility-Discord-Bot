043b0761aff11b21cf92152af0dfd4f227509df2
Michael Yaworski <mikeyaworski@hotmail.com>
2020-12-28 03:25:25 -0500

    Added confirmation to !move command

--- src/commands/utilities/move.ts
+++ src/commands/utilities/move.ts
@@ -1,10 +1,10 @@
-import type { ClientType, CommandRunMethod } from 'src/types';
 import type { Message, TextChannel } from 'discord.js';
 import type { CommandoMessage } from 'discord.js-commando';
+import type { ClientType, CommandBeforeConfirmMethod, CommandAfterConfirmMethod, EitherMessage } from 'src/types';
 
 import Discord from 'discord.js';
-import { Command } from 'discord.js-commando';
 import { getMessagesInRange, userHasPermission } from 'src/discord-utils';
+import ConfirmationCommand from 'src/commands/confirmation-command';
 
 interface Args {
   channel: TextChannel,
@@ -12,10 +12,12 @@ interface Args {
   end: CommandoMessage | false,
 }
 
+type IntermediateResult = EitherMessage[];
+
 /**
  * !move <channel> <start_msg> [end_msg]
  */
-export default class MoveCommand extends Command {
+export default class MoveCommand extends ConfirmationCommand<IntermediateResult> {
   constructor(client: ClientType) {
     super(client, {
       name: 'move',
@@ -63,30 +65,42 @@ export default class MoveCommand extends Command {
     await msg.delete();
   }
 
-  run: CommandRunMethod<Args> = async (commandMsg, args) => {
+  beforeConfirm: CommandBeforeConfirmMethod<Args, IntermediateResult> = async (commandMsg, args) => {
     const { channel: toChannel, start, end } = args;
     const fromChannel = start.channel;
     if (toChannel.id === fromChannel.id) {
-      return commandMsg.reply('You\'re moving messages to the same channel??');
+      await commandMsg.reply('You\'re moving messages to the same channel??');
+      return null;
     }
 
     // It would be nice to use the hasPermission instance function, but that does not give us access to the resolved arguments
     // (we get strings instead of the resolved message/channel objects). So we check it here, in the run operation.
     if (!userHasPermission(toChannel, start.author, ['SEND_MESSAGES', 'VIEW_CHANNEL'])) {
-      return commandMsg.reply(`You do not have access to send messages in <#${toChannel.id}>`);
+      await commandMsg.reply(`You do not have access to send messages in <#${toChannel.id}>`);
+      return null;
     }
-
-    toChannel.startTyping();
-    await toChannel.send(`__Messages moved from__ <#${fromChannel.id}>`);
 
     // single message; not a range
     if (!end) {
+      toChannel.startTyping();
+      await toChannel.send(`__Messages moved from__ <#${fromChannel.id}>`);
       await MoveCommand.moveMessage(toChannel, start);
       toChannel.stopTyping(true);
-      return commandMsg.delete();
+      await commandMsg.delete();
+      return null;
     }
 
     const msgs = await getMessagesInRange(fromChannel, start, end);
+
+    return [msgs, `Are you sure you want to move ${msgs.length} messages to <#${toChannel.id}>`];
+  }
+
+  afterConfirm: CommandAfterConfirmMethod<Args, IntermediateResult> = async (msgs, commandMsg, args) => {
+    const { channel: toChannel, start } = args;
+    const fromChannel = start.channel;
+
+    toChannel.startTyping();
+    await toChannel.send(`__Messages moved from__ <#${fromChannel.id}>`);
 
     // do these in order
     for (let i = 0; i < msgs.length; i++) {
@@ -95,6 +109,6 @@ export default class MoveCommand extends Command {
     }
 
     toChannel.stopTyping(true);
-    return commandMsg.delete();
+    return `${msgs.length} messages have been moved to <#${toChannel.id}>`;
   }
 }
