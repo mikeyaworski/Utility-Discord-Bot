b4f59b95ff3a38f6feafdb1ac8857d42942415ad
Michael Yaworski <mikeyaworski@hotmail.com>
2021-01-09 20:56:33 -0500

    Improved unique reaction role consistency

--- src/events/reaction-roles.ts
+++ src/events/reaction-roles.ts
@@ -23,40 +23,70 @@ async function reactionReady(messageReaction: MessageReaction): Promise<boolean>
 const ReactionAddEvent: EventTrigger = ['messageReactionAdd', async (messageReaction: MessageReaction, user: User): Promise<void> => {
   if (user.bot) return;
   if (!(await reactionReady(messageReaction))) return;
+
   const guildId = messageReaction.message.guild!.id;
   const messageId = messageReaction.message.id;
   const member = await messageReaction.message.guild!.members.fetch({
     user,
     force: true,
   });
+
+  type UniqueRule = {
+    unique: boolean;
+  }
+  type Rule = {
+    role_id: string;
+    emoji: string;
+  }
+
   const [uniqueRule, rules] = await Promise.all([
     getModels().reaction_messages_unique.findOne({
       where: {
         guild_id: guildId,
         message_id: messageId,
       },
-    }),
+      attributes: ['unique'],
+    }) as Promise<UniqueRule | null>,
     getModels().reaction_roles.findAll({
       where: {
         guild_id: guildId,
         message_id: messageId,
-        emoji: messageReaction.emoji.toString(),
       },
-      attributes: ['role_id'],
-    }),
+      attributes: ['role_id', 'emoji'],
+    }) as Promise<Rule[]>,
   ]);
-  const roleIds: string[] = rules.map(rule => rule.role_id);
+
+  const rolesToAdd = rules.filter(rule => rule.emoji === messageReaction.emoji.toString()).map(rule => rule.role_id);
+  const emojisToUnreact = rules.filter(rule => rule.emoji !== messageReaction.emoji.toString()).map(rule => rule.emoji);
+
   try {
-    await member.roles.add(roleIds);
+    await member.roles.add(rolesToAdd);
   } catch (err) {
     // likely to happen if the role trying to be given is higher than the bot's role
     error(err);
   }
+
   if (uniqueRule?.unique) {
-    const otherReactions = messageReaction.message.reactions.cache.filter(reaction => reaction.emoji.toString() !== messageReaction.emoji.toString());
-    otherReactions.forEach(otherReaction => {
+    // Update the cache of users for each reaction on the message.
+    // It would be awesome to do this as a job when the bot spins up, but doing the fetches
+    // at that time will not update the cache used here for some reason... weird API.
+    await Promise.all(messageReaction.message.reactions.cache.map(async reaction => {
+      if (!reaction.users.cache.size) {
+        // Note: this only works for the first 100 users
+        await reaction.users.fetch();
+      }
+    }));
+
+    emojisToUnreact.forEach(emoji => {
+      const reaction = messageReaction.message.reactions.resolve(emoji);
+      if (!reaction) return;
       try {
-        if (otherReaction.users.cache.has(user.id)) otherReaction.users.remove(user);
+        // If the cache is populated, let's reduce API calls by comparing against the cache.
+        // The cache should be populated since we populate it above.
+        // But if the cache is empty, let's just force remove the reaction (potentially wasting an API call, but we don't know for sure).
+        if (!reaction.users.cache.size || reaction.users.cache.has(user.id)) {
+          reaction.users.remove(user);
+        }
       } catch (err) {
         error(err);
       }
