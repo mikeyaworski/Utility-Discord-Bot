6e6c8290769937ce3d86c45fa221ddb073f4ecce
Michael Yaworski <mikeyaworski@hotmail.com>
2021-07-10 23:41:36 -0400

    Support deleting messages in other channels

--- src/commands/utilities/delete.ts
+++ src/commands/utilities/delete.ts
@@ -1,20 +1,22 @@
-import type { CommandoMessage } from 'discord.js-commando';
-import type { TextChannel } from 'discord.js';
+import type { Message, NewsChannel, TextChannel } from 'discord.js';
 import type { ClientType, CommandBeforeConfirmMethod, CommandAfterConfirmMethod, EitherMessage } from 'src/types';
 
 import chunk from 'lodash.chunk';
 
 import { BULK_MESSAGES_LIMIT } from 'src/constants';
 import ConfirmationCommand, { DEFAULT_CONFIRMATION_INFO } from 'src/commands/confirmation-command';
-import { getMessagesInRange } from 'src/discord-utils';
+import { getMessagesInRange, findMessageInGuild, userHasPermission } from 'src/discord-utils';
 
 type Args = {
-  start: CommandoMessage,
-  end: CommandoMessage | false,
-  old: boolean;
+  startId: string,
+  endId: string,
+  isOld: boolean;
 };
 
-type IntermediateResult = EitherMessage[];
+type IntermediateResult = {
+  msgs: EitherMessage[],
+  channel: TextChannel | NewsChannel,
+};
 
 /**
  * !delete <start_msg> [end_msg] [isOld]
@@ -31,9 +33,7 @@ export default class DeleteCommand extends ConfirmationCommand<Args, Intermediat
         '!delete 784701167241658380 784701171147341834',
         '!delete 784701167241658380',
       ],
-      format: '<start_msg> [end_msg] [isOld]',
-      userPermissions: ['MANAGE_MESSAGES'],
-      clientPermissions: ['MANAGE_MESSAGES'],
+      format: '<start_msg_id> [end_msg_id] [isOld]',
       guildOnly: true,
       throttling: {
         usages: 2,
@@ -41,19 +41,18 @@ export default class DeleteCommand extends ConfirmationCommand<Args, Intermediat
       },
       args: [
         {
-          key: 'start',
+          key: 'startId',
           prompt: 'Message ID for the starting message.',
-          type: 'message',
+          type: 'string',
         },
         {
-          key: 'end',
+          key: 'endId',
           prompt: '(Optional) Message ID for the ending message (creates a range). Leave blank to only delete the starting message.',
-          type: 'message',
-          // you can't do null... LOL
-          default: false,
+          type: 'string',
+          default: '',
         },
         {
-          key: 'old',
+          key: 'isOld',
           prompt: '(Optional) Whether or not the messages will be over two weeks old (requires different API calls).',
           type: 'boolean',
           default: false,
@@ -66,37 +65,55 @@ export default class DeleteCommand extends ConfirmationCommand<Args, Intermediat
   }
 
   beforeConfirm: CommandBeforeConfirmMethod<Args, IntermediateResult> = async (commandMsg, args) => {
-    const { start, end } = args;
-    const { channel } = start;
+    const { startId, endId } = args;
+    const [startMsg, channel] = await findMessageInGuild(
+      startId,
+      commandMsg.guild,
+      // We know it's a text channel since this is a guild-only command
+      commandMsg.channel as TextChannel | NewsChannel,
+    );
+    if (!startMsg || !channel) {
+      await commandMsg.reply('Could not find starting message.');
+      return null;
+    }
 
-    if (channel.type === 'dm') return null;
+    if (!userHasPermission(channel, commandMsg.author, ['MANAGE_MESSAGES'])) {
+      await commandMsg.reply(`You do not have permission to delete messages in <#${channel.id}>`);
+      return null;
+    }
 
     // single message; not a range
-    if (!end) {
-      await start.delete();
+    if (!endId) {
+      await startMsg.delete();
       await commandMsg.delete();
       return null;
     }
 
-    const [msgs, stoppedEarly] = await getMessagesInRange(channel, start, end);
-    const confirmPrompt = `Are you sure you want to delete ${msgs.length} messages?${
+    let endMsg: Message;
+    try {
+      endMsg = await channel.messages.fetch(endId);
+    } catch (err) {
+      await commandMsg.reply('End message is not in the same channel as start message.');
+      return null;
+    }
+
+    const [msgs, stoppedEarly] = await getMessagesInRange(channel, startMsg, endMsg);
+    const confirmPrompt = `Are you sure you want to delete ${msgs.length} messages in <#${channel.id}>?${
       stoppedEarly ? '\nNote: Some messages in the range were not included due to a rate limit precaution.' : ''
     }`;
-    return [msgs, confirmPrompt];
+    return [{ msgs, channel }, confirmPrompt];
   }
 
-  afterConfirm: CommandAfterConfirmMethod<Args, IntermediateResult> = async (msgs, commandMsg, args) => {
-    const { start, old } = args;
-    const { channel } = start;
-    let numDeletedMessages: number = msgs.length;
-    if (!old) {
+  afterConfirm: CommandAfterConfirmMethod<Args, IntermediateResult> = async (result, commandMsg, args) => {
+    const { msgs, channel } = result;
+    const { isOld } = args;
+    let numDeletedMessages = msgs.length;
+    if (!isOld) {
       const msgIds = msgs.map(msg => msg.id);
-      // we know it is a text channel since it otherwise would not have passed beforeConfirm
-      const textChannel = channel as TextChannel;
       const chunkedMsgIds = chunk(msgIds, BULK_MESSAGES_LIMIT);
       numDeletedMessages = 0;
       await Promise.all(chunkedMsgIds.map(async msgIdsChunk => {
-        numDeletedMessages += (await textChannel.bulkDelete(msgIdsChunk)).size;
+        numDeletedMessages += (await channel.bulkDelete(msgIdsChunk)).size;
       }));
     } else {
       await Promise.all(msgs.map(msg => msg.delete()));
