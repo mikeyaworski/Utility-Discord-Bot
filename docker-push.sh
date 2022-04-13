58dd75869f66e7e293668f4ec0d8d17361a5db93
Michael Yaworski <mikeyaworski@hotmail.com>
2022-04-13 10:13:38 -0400

    Update docker push script to build for multiple architectures

--- docker-push.sh
+++ docker-push.sh
@@ -2,5 +2,11 @@
 tag=${1:-$(git rev-parse --short HEAD)}
 imageName="mikeyaworski/utility-discord-bot:$tag"
 
-docker build -t $imageName .
-docker push $imageName
+# You may need to run: docker buildx create --use
+# if this is your first time using buildx
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $imageName --push .
+
+# If you don't care to support multiple platform architectures,
+# then use these commands instead of buildx:
+# docker build -t $imageName .
+# docker push $imageName
