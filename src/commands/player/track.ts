773cea6dfa0801b68fc3f7e2f36c927dfbe71d43
Michael Yaworski <mikeyaworski@hotmail.com>
2022-07-22 04:57:25 -0400

    Uses youtube-dl-exec instead of ytdl-core

--- src/commands/player/track.ts
+++ src/commands/player/track.ts
@@ -1,5 +1,6 @@
 import { AudioResource, createAudioResource } from '@discordjs/voice';
 
+import { exec as ytdlExec } from 'youtube-dl-exec';
 import ffmpeg from 'fluent-ffmpeg';
 import play from 'play-dl';
 import ytdl from 'ytdl-core';
@@ -50,6 +51,71 @@ export default class Track {
       return audioResource;
     };
 
+    const tryYtdlExec: () => Promise<AudioResource<Track>> = async () => {
+      // https://github.com/discordjs/voice/blob/f1869a9af5a44ec9a4f52c2dd282352b1521427d/examples/music-bot/src/music/track.ts#L46-L76
+      return new Promise((resolve, reject) => {
+        const process = ytdlExec(
+          this.link, {
+            output: '-',
+            quiet: true,
+            format: 'bestaudio',
+            limitRate: '100K',
+          }, {
+            stdio: ['ignore', 'pipe', 'ignore'],
+          },
+        );
+        if (!process.stdout) {
+          reject(new Error('No stdout'));
+          return;
+        }
+        const stream = process.stdout;
+        const onError = (err: unknown) => {
+          if (!process.killed) process.kill();
+          stream.resume();
+          reject(err);
+          // This ERR_STREAM_PREMATURE_CLOSE error happens when you skip the last song, but there is no issue with that.
+          // TODO: See if there is an alternative way to skip songs which does not run into this error.
+          // @ts-ignore This is useless TS
+          if (typeof err === 'object' && err && 'code' in err && err.code === 'ERR_STREAM_PREMATURE_CLOSE') {
+            return;
+          }
+          error(err);
+        };
+        process.once('spawn', () => {
+          const manipulatedStream = ffmpeg({ source: stream }).toFormat('mp3');
+          if (speed) {
+            manipulatedStream.audioFilters(`atempo=${speed}`);
+          }
+          if (seek) {
+            manipulatedStream.setStartTime(Math.ceil(seek));
+          }
+          // @ts-expect-error This actually works
+          return resolve(createAudioResource(manipulatedStream, { metadata: this }));
+        }).catch(onError);
+      });
+    };
+
+    /**
+     * This is currently unused, but may be useful in the future.
+     * For now, it's the least consistent method since some videos do not work for whatever reason.
+     * This is probably faster than youtube-dl-exec, so it would be nice to use this instead if possible.
+     */
+    const tryYtdlCore: () => Promise<AudioResource<Track>> = async () => {
+      const stream = ytdl(this.link);
+      if (!seek && !speed) {
+        return createAudioResource(stream, { metadata: this });
+      }
+      const manipulatedStream = ffmpeg({ source: stream }).toFormat('mp3');
+      if (speed) {
+        manipulatedStream.audioFilters(`atempo=${speed}`);
+      }
+      if (seek) {
+        manipulatedStream.setStartTime(Math.ceil(seek));
+      }
+      // @ts-expect-error This actually works
+      return createAudioResource(manipulatedStream, { metadata: this });
+    };
+
     switch (this.variant) {
       case TrackVariant.YOUTUBE_LIVESTREAM:
       case TrackVariant.YOUTUBE_VOD: {
@@ -65,19 +131,7 @@ export default class Track {
       // We want to fall through if play-dl doesn't work, or the speed option is provided
       // eslint-disable-next-line no-fallthrough
       default: {
-        const stream = ytdl(this.link);
-        if (!seek && !speed) {
-          return createAudioResource(stream, { metadata: this });
-        }
-        const manipulatedStream = ffmpeg({ source: stream }).toFormat('mp3');
-        if (seek) {
-          manipulatedStream.setStartTime(Math.ceil(seek));
-        }
-        if (speed) {
-          manipulatedStream.audioFilters(`atempo=${speed}`);
-        }
-        // @ts-expect-error This actually works
-        return createAudioResource(manipulatedStream, { metadata: this });
+        return tryYtdlExec();
       }
     }
   }
