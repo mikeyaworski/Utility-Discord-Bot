769a25693adbfb777bb6b9f1dea19b5f302c7077
Michael Yaworski <mikeyaworski@hotmail.com>
2022-08-18 03:54:48 -0400

    Improves fetching bot DM channel by creating one if not already cached

--- src/api/routes/dms.ts
+++ src/api/routes/dms.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import authMiddleware, { AuthRequest } from 'src/api/middlewares/auth';
 import { client } from 'client';
-import { ChannelType } from 'discord.js';
 
 const router = express.Router();
 
 // @ts-expect-error
 router.get('/channel', authMiddleware, async (req: AuthRequest, res) => {
-  const dmChannel = client.channels.cache.find(channel => {
-    return channel.type === ChannelType.DM && channel.recipientId === req.user.id;
-  });
-  if (!dmChannel) return res.status(404).end();
+  const user = await client.users.fetch(req.user.id, { force: true });
+  if (!user) return res.status(404).send('Could not find user');
+  let dmChannel = user.dmChannel;
+  if (!dmChannel) dmChannel = await user.createDM();
+  if (!dmChannel) return res.status(400).send('Could not find or create DM channel');
   return res.status(200).json({
     id: dmChannel.id,
   });
