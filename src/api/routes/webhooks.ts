2191692f0850b335b0b2eed95caf940f1e72e8fd
DESKTOP-V3FPRIF\mikeyaworski <mikeyaworski@hotmail.com>
2023-01-21 12:32:26 -0500

    Adds webhook endpoint to send DM

--- src/api/routes/webhooks.ts
+++ src/api/routes/webhooks.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import { WebhookClient } from 'discord.js';
 import { log, error } from 'src/logging';
+import { getDmChannel } from './dms';
 
 dotenv.config();
 
@@ -28,6 +29,26 @@ router.post('/', async (req, res) => {
     // TODO: Pass the status code and message from the Discord error if available
     error(err);
     return res.status(500).end();
+  }
+});
+
+router.post('/dm', async (req, res) => {
+  const isAuthorized = webhookSecret && webhookSecret === req.get('X-WEBHOOK-SECRET');
+  if (!isAuthorized) return res.status(401).end();
+
+  const { userId, data } = req.body;
+  if (!userId || !data) return res.status(400).send('userId and data are required.');
+
+  log('Received webhook DM request:', userId, data);
+
+  try {
+    const dmChannel = await getDmChannel(userId);
+    if (!dmChannel) return res.status(400).send('Could not find or create DM channel');
+    await dmChannel.send(data);
+    log('Sent DM from webhook:', userId, data);
+    return res.status(204).end();
+  } catch (err) {
+    return res.status(404).send(String(err));
   }
 });
 
