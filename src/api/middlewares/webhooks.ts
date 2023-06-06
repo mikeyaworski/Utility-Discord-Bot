630b346e9564a0d172c00de00b3fbccc9e70c660
DESKTOP-V3FPRIF\mikeyaworski <mikeyaworski@hotmail.com>
2023-06-06 18:45:50 -0400

    Adds support to ignore webhook requests if it contains certain text
    * Refactors webhookAuthMiddleware export
    * Adds middleware to check required fields
    * Adds middleware to check if the webhook should be ignored
    * Adds support for "ignoredText" field in request body

--- src/api/middlewares/webhooks.ts
+++ src/api/middlewares/webhooks.ts
@@ -1,18 +1,70 @@
 import type { Request, Response, NextFunction } from 'express';
+import type { MessageCreateOptions, MessagePayload, WebhookMessageCreateOptions } from 'discord.js';
 import dotenv from 'dotenv';
+
+import { log } from 'src/logging';
 
 dotenv.config();
 
 const webhookSecret = process.env.WEBHOOK_SECRET;
 
-export default async function webhookAuthMiddleware(
+type Data = string | MessagePayload | MessageCreateOptions | WebhookMessageCreateOptions;
+
+export function webhookAuthMiddleware(
   req: Request,
   res: Response,
   next: NextFunction,
-): Promise<void> {
+): void {
   const isAuthorizedThroughHeader = webhookSecret === req.get('X-WEBHOOK-SECRET');
   const isAuthorizedThroughBody = webhookSecret === req.body.webhookSecret;
   const isAuthorized = webhookSecret && (isAuthorizedThroughHeader || isAuthorizedThroughBody);
   if (!isAuthorized) res.status(401).end();
   else next();
+}
+
+export const requiredFieldsMiddleware = (fields: string[]) => (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  const missingRequiredField = fields.some(field => !req.body[field]);
+  if (missingRequiredField) res.status(400).send(`All fields are required: ${fields.join(', ')}`);
+  else next();
+};
+
+export function skipRequestMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const ignoreText: string | undefined = req.body.ignoreText;
+  if (!ignoreText) {
+    next();
+    return;
+  }
+
+  const data: Data = req.body.data;
+
+  const contentToCheck: string[] = [];
+  if (typeof data === 'string') {
+    contentToCheck.push(data);
+  } else {
+    if ('content' in data && data.content) {
+      contentToCheck.push(data.content);
+    }
+    if ('embeds' in data && data.embeds) {
+      contentToCheck.push(...data.embeds.map(embed => {
+        const e = 'toJSON' in embed ? embed.toJSON() : embed;
+        return [e.title || '', e.description || ''];
+      }).flat());
+    }
+  }
+
+  const containsIgnoredText = contentToCheck.some(value => value.toLowerCase().includes(ignoreText.toLowerCase()));
+  if (containsIgnoredText) {
+    log(`Ignored webhook request because content contains: "${ignoreText}"\n${JSON.stringify(req.body)}`);
+    res.status(204).end();
+  } else {
+    next();
+  }
 }
--- src/api/routes/webhooks.ts
+++ src/api/routes/webhooks.ts
@@ -1,7 +1,7 @@
 import express, { Response } from 'express';
 import { WebhookClient } from 'discord.js';
 import get from 'lodash.get';
-import webhookAuthMiddleware from 'src/api/middlewares/webhooks';
+import { webhookAuthMiddleware, requiredFieldsMiddleware, skipRequestMiddleware } from 'src/api/middlewares/webhooks';
 import { log, error } from 'src/logging';
 import { getDmChannel } from './dms';
 
@@ -17,37 +17,43 @@ function handleError(err: unknown, res: Response) {
 }
 
 // https://discord.com/api/webhooks/{webhookId}/{webhookToken}
-router.post('/', webhookAuthMiddleware, async (req, res) => {
-  const { webhookId, webhookToken, data } = req.body;
-  if (!webhookId || !webhookToken || !data) return res.status(400).send('webhookId, webhookToken and data are required.');
+router.post(
+  '/',
+  webhookAuthMiddleware,
+  requiredFieldsMiddleware(['webhookId', 'webhookToken', 'data']),
+  skipRequestMiddleware,
+  async (req, res) => {
+    const { webhookId, webhookToken, data } = req.body;
+    log('Received webhook request:', webhookId, data);
+    try {
+      const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
+      await webhookClient.send(data);
+      log('Sent message from webhook:', webhookId, data);
+      return res.status(204).end();
+    } catch (err) {
+      return handleError(err, res);
+    }
+  },
+);
 
-  log('Received webhook request:', webhookId, data);
-
-  try {
-    const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
-    await webhookClient.send(data);
-    log('Sent message from webhook:', webhookId, data);
-    return res.status(204).end();
-  } catch (err) {
-    return handleError(err, res);
-  }
-});
-
-router.post('/dm', webhookAuthMiddleware, async (req, res) => {
-  const { userId, data } = req.body;
-  if (!userId || !data) return res.status(400).send('userId and data are required.');
-
-  log('Received webhook DM request:', userId, data);
-
-  try {
-    const dmChannel = await getDmChannel(userId);
-    if (!dmChannel) return res.status(400).send('Could not find or create DM channel');
-    await dmChannel.send(data);
-    log('Sent DM from webhook:', userId, data);
-    return res.status(204).end();
-  } catch (err) {
-    return handleError(err, res);
-  }
-});
+router.post(
+  '/dm',
+  webhookAuthMiddleware,
+  requiredFieldsMiddleware(['userId', 'data']),
+  skipRequestMiddleware,
+  async (req, res) => {
+    const { userId, data } = req.body;
+    log('Received webhook DM request:', userId, data);
+    try {
+      const dmChannel = await getDmChannel(userId);
+      if (!dmChannel) return res.status(400).send('Could not find or create DM channel');
+      await dmChannel.send(data);
+      log('Sent DM from webhook:', userId, data);
+      return res.status(204).end();
+    } catch (err) {
+      return handleError(err, res);
+    }
+  },
+);
 
 export default router;
